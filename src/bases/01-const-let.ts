c9925f0be05e2fd085e50705904214bc51ed5d65
Sebasss <sebas.cruz750@gmail.com>
2024-09-13 20:24:01 -0500

    initial Commit

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,14 @@
+
+// var ya no se usa, es anticuado
+const firstName  = "Sebas"
+const lastName  = "Cruz"
+
+// firstName = "Sebas" // hay un error
+
+// usar const en su mayoría o let
+
+let middleName = "Sebass"
+
+middleName = "Sebas"
+
+console.log({firstName,middleName,lastName})
